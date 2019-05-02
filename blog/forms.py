de8bf7df1a301a83171942eb2775fbd9b77d0597
jaehun <kimjaehun96@gmail.com>
2019-05-02 12:41:37 +0900

    book_review

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Post, Comment
+
+class PostForm(forms.ModelForm): #ModelForm 임을 알려주는 구문.
+    class Meta:
+        model = Post
+        fields = ('title', 'content', 'price', 'rating',)
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ( 'content' , )
---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+# from django.utils
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_date = models.DateTimeField( default= timezone.now)
+    price = models.IntegerField(null=True)
+    score = ((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'))
+    rating=models.IntegerField(
+        
+        choices=score, 
+        default=1,
+    )
+    def __str__(self):
+        return self.title
+        
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete = models.CASCADE, related_name='comments') #댓글이 속한 글이 지워지면 댓글들도 다 지우도록 함.
+    content = models.TextField()
